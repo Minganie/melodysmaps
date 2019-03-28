54efafdef59a37778c8ad4b3cf5d0ff4658910d8
Myriam Luce <luce.myriam@gmail.com>
2019-03-28 15:31:42 -0400

    Finished the non-breaking changes to db to transfer to xiv api location.

--- changes.sql
+++ changes.sql
@@ -79,16 +79,11 @@ FROM nondropping AS nd
 WHERE lower(ms.name)=lower(nd.name) AND st_intersects(ms.geom, nd.geom);
 
 -- Hunting grounds
---Figure out which Jackanapes is right??
---Verify Watchwolf fate status in NT
---Verify Yarzon Scavenger NS level
---Basically, check everything that comes up with that query down
-
--- same name, same level, same zone, geom intersects
+-- After fixing my human mistakes, all hg's are in mob_spawns
+-- EXCEPT those FATE-dependent mobs that we will add now...
 with hgg as
 (select gid, level, name, case when substring(name from '^(.+?)\(.+') is null then name else trim(substring(name from '^(.+)\(.+')) end as namepart, requires, geom, nkilled, elite, agressive
 from hunting_grounds),
-
 joined as 
 (select ms.gid as msgid, ms.name as msname, ms.level, ms.zone, hgg.level, hgg.namepart as hgname, hgg.gid as hggid
 from mob_spawns as ms
@@ -98,19 +93,60 @@ from mob_spawns as ms
         ms.zone=(select z.name from zones as z where st_contains(z.geom, hgg.geom)) and
         st_intersects(ms.geom, hgg.geom)
 order by ms.name, hgg.namepart, ms.zone, ms.level),
-
--- hg not joined
 hg_not_joined as (
 select *
 from hgg
 where hgg.gid not in (select hggid from joined)
 order by name)
+INSERT INTO mob_spawns (name, zone, level, is_fate, agressive, elite, requires, geom)
+select hg.namepart, 
+    (select z.name from zones as z where st_contains(z.geom, hg.geom)), 
+    hg.level, 
+    true, 
+    hg.agressive, 
+    hg.elite, 
+    hg.requires, 
+    st_buffer(st_centroid(hg.geom), 0.07)
+from hg_not_joined as hg;
 
-select hg.level, hg.namepart, ms.name, ms.level, hg.requires
-from hg_not_joined as hg
-left join mob_spawns as ms on lower(hg.namepart)=lower(ms.name);
+-- NOW copy info from hg to mob_spawns
+ALTER TABLE mob_spawns ADD COLUMN nkilled integer CHECK(nkilled>=0) NOT NULL DEFAULT 0;
 
--- same name, different geometry?
-select nd.name, ms.name
-from nd_not_joined as nd
-left join mob_spawns as ms on nd.name=ms.name;
+with hgg as
+(select gid, level, name, case when substring(name from '^(.+?)\(.+') is null then name else trim(substring(name from '^(.+)\(.+')) end as namepart, requires, geom, nkilled, elite, agressive
+from hunting_grounds)
+UPDATE mob_spawns AS ms SET agressive=hgg.agressive, elite=hgg.agressive, requires=hgg.requires, nkilled=hgg.nkilled
+FROM hgg
+WHERE hgg.gid is not null and ms.gid is not null and
+        lower(ms.name)=lower(hgg.namepart) 
+        and ms.level=hgg.level
+        and ((hgg.requires is not null and is_fate)
+            or (hgg.requires is null and not is_fate))
+        and st_intersects(ms.geom, hgg.geom);
+
+-- TRANSFER the foreign key in hunted_where from hunting_grounds to mob_spawns
+ALTER TABLE hunted_where ADD COLUMN hg integer REFERENCES mob_spawns(gid);
+
+with hgg as
+(select gid, level, name, case when substring(name from '^(.+?)\(.+') is null then name else trim(substring(name from '^(.+)\(.+')) end as namepart, requires, geom, nkilled, elite, agressive
+from hunting_grounds),
+ms AS (
+select ms.gid, hgg.name as node
+from hgg
+    join mob_spawns as ms ON
+        lower(ms.name)=lower(hgg.namepart) 
+        and ms.level=hgg.level
+        and ((hgg.requires is not null and is_fate)
+            or (hgg.requires is null and not is_fate))
+        and st_intersects(ms.geom, hgg.geom)
+)
+UPDATE hunted_where AS hw SET hg=ms.gid
+FROM ms 
+    WHERE hw.node=ms.node;
+    
+select hw.node, hw.itemlid, hw.hg, ms.name, ms.level, ms.is_fate
+from hunted_where as hw
+    left join mob_spawns as ms ON hw.hg=ms.gid;
+
+alter table hunted_where drop constraint hunted_where_pkey, add constraint hunted_where_pkey PRIMARY KEY (hg, itemlid);
+alter table hunted_where add constraint hunted_where_tmp_ukey UNIQUE (node, itemlid);
