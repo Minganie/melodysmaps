7187c56bd4223e1367a9df28918299124d2315d7
mluce <luce.myriam@gmail.com>
2019-08-27 21:50:16 -0400

    Added table for BLU quest requirements. sigh.

--- changes.sql
+++ changes.sql
@@ -1,6 +1,13 @@
 -- pg_dump -F c -f ffxiv20190708.backup ffxiv
 -- cd D:\Programmes\Postgres\11\bin
 -- cd C:\Program Files\PostgreSQL\11\bin
--- pg_restore.exe -U postgres -d postgres --clean --create D:\Programmes\xampp\htdocs\melodysmaps\ffxiv20190708.backup
+-- pg_restore.exe -U postgres -d postgres --clean --create D:\Programmes\xampp\htdocs\melodysmaps\ffxiv20190716.backup
 -- pg_restore.exe -U postgres -d postgres --clean --create C:\xampp\htdocs\melodysmaps\ffxiv20190712.backup
 
+CREATE TABLE quest_action_requirements (
+    questlid text references quests(lid),
+    action text references requirements(name),
+    primary key (questlid, action)
+);
+GRANT SELECT ON quest_action_requirements TO ffxivro;
+GRANT INSERT, UPDATE, DELETE ON quest_action_requirements TO ffxivrw;
