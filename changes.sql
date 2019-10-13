461309a99d7ee4fd16b6b546432458e7d1a24351
Myriam Luce <myriam@yombu.com>
2019-10-13 11:22:53 -0400

    Added tabs to the levemete popup

--- changes.sql
+++ changes.sql
@@ -4,3 +4,27 @@
 -- pg_restore.exe -U postgres -d postgres --clean --create D:\Programmes\xampp\htdocs\melodysmaps\ffxiv20190716.backup
 -- pg_restore.exe -U postgres -d postgres --clean --create C:\xampp\htdocs\melodysmaps\ffxiv20190829.backup
 
+CREATE OR REPLACE FUNCTION ffxiv.get_leves(
+	levemete text)
+    RETURNS json
+    LANGUAGE 'sql'
+
+    COST 100
+    STABLE 
+AS $BODY$
+
+select json_object_agg(type, leves) 
+from
+	(
+		select type, json_agg(leve) as leves 
+		from
+			(
+				select type, get_leve(name) as leve
+				from leves
+				where levemete=$1
+				order by job, lvl, name
+			)a
+		group by type
+	)b
+
+$BODY$;
--- js/Selectable.Levemete.js
+++ js/Selectable.Levemete.js
@@ -10,24 +10,51 @@ Selectable.Levemete.prototype = $.extend({}, Selectable.DefaultPoint.prototype, 
         return $("<h2>" + (popupable.settlement ? popupable.settlement : '?' ) + " (lvl " + (popupable.min_lvl ? popupable.min_lvl : '?') + "-" + (popupable.max_lvl ? popupable.max_lvl : '?') + ")</h2>");
     },
 	_getPopupContent: function(popupable) {
-		var html = $('<table></table>')
-            .addClass('melsmaps-levemete');
+		var html = $('<div class="melsmaps-merchant-popup"></div>');
+		var tabList = $('<ul></ul>')
+            .appendTo(html);
+		var first = true;
+		var c = 0;
 		if(popupable && popupable.leves) {
-			for(var i in popupable.leves) {
-                var lev = popupable.leves[i];
-				var leve = Selectable.getFull(lev);
-				// console.log(leve);
-                var tr = $('<tr></tr>')
-                    .addClass('melsmaps-is-a-leve-tooltip')
-                    .attr('data-melsmaps-tooltip', Selectable.getLeveTooltip(lev).prop('outerHTML'))
-                    .appendTo(html);
-                $('<td></td>')
-                    .append(leve.getIcon(lev))
-                    .appendTo(tr);
-                $('<td>' + lev.name + '</td>')
-                    .appendTo(tr);
-                $('<td>(lvl ' + lev.lvl + ')</td>')
-                    .appendTo(tr);
+			for(var cat in popupable.leves) {
+                var leves = popupable.leves[cat];
+				
+				// MAKE TAB
+				var available = ($.isEmptyObject(leves) ? 'unavailable' : 'available');
+				if(first && available == 'available') {
+					var active = ' active';
+					first = false;
+				} else
+					var active = '';
+				var tabHtml = $('<div></div>')
+					.addClass('tab-content tab-content-' + c + active)
+					.appendTo(html);
+				$('<li></li>')
+					.addClass(available + active + ' tab-name-' + c)
+					.html(cat)
+					.appendTo(tabList);
+				var table = $('<table></table>')
+					.addClass('melsmaps-levemete')
+					.appendTo(tabHtml);
+					
+				// FILL TAB
+				for(var i in leves) {
+					var full = leves[i];
+					var leve = Selectable.getFull(full);
+					// console.log(leve);
+					var tr = $('<tr></tr>')
+						.addClass('melsmaps-is-a-leve-tooltip')
+						.attr('data-melsmaps-tooltip', Selectable.getLeveTooltip(full).prop('outerHTML'))
+						.appendTo(table);
+					$('<td></td>')
+						.append(leve.getIcon(full))
+						.appendTo(tr);
+					$('<td>' + full.name + '</td>')
+						.appendTo(tr);
+					$('<td>(lvl ' + full.lvl + ')</td>')
+						.appendTo(tr);
+				}
+				c++;
 			}
 		}
 		return html;
