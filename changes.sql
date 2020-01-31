7ee106078bd97b4d68bfe78220b0a0515aefbec9
Myriam Luce <luce.myriam@gmail.com>
2020-01-31 14:06:47 -0500

    Reset db changes

--- changes.sql
+++ changes.sql
@@ -3,7 +3,3 @@
 -- cd C:\Program Files\PostgreSQL\11\bin
 -- pg_restore.exe -U postgres -d postgres --clean --create D:\Programmes\xampp\htdocs\melodysmaps\ffxiv20190716.backup
 -- pg_restore.exe -U postgres -d postgres --clean --create C:\xampp\htdocs\melodysmaps\ffxiv20190829.backup
-
-DROP TRIGGER add_pvp_lid ON ffxiv.pvps;
-DROP TRIGGER replace_pvp_lid ON ffxiv.pvps;
-INSERT INTO duty_types (name) VALUES ('Ultimate Raids');
