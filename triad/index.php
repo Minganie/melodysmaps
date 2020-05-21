69c166c32f35c88af25d60a203586df571651eed
mluce <luce.myriam@gmail.com>
2020-05-21 16:22:50 -0400

    In the process of moving over to twig templates and fast-route

---
+++ triad/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__);
+$twig = new \Twig\Environment($loader, [
+    'debug' => true,
+    'cache' => __DIR__ . '/cache',
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) use ($twig) {
+    $r->get('/melodysmaps/triad/', function() use ($twig) {
+        $string = file_get_contents(__DIR__ . '/plays/Caribou.json');
+        $play = json_decode($string, true);
+        foreach($play['leftBoards'] as $i => $board) {
+            $string = file_get_contents(__DIR__ . "/boards/$board.json");
+            $play['leftBoards'][$i] = json_decode($string, true);
+        }
+        foreach($play['rightBoards'] as $i => $board) {
+            $string = file_get_contents(__DIR__ . "/boards/$board.json");
+            $play['rightBoards'][$i] = json_decode($string, true);
+        }
+        error_log(print_r($play, true));
+        echo $twig->render('twig.html', ['play' => $play]);
+    });
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        die('Not Found');
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        die('Not Allowed');
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        print $handler($vars);
+        break;
+}
